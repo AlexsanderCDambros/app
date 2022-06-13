dd2c3723415ef4240ad53d0ba5a297b7dac30c39
AlexsanderCDambros <alexsanderdambros@gmail.com>
2022-06-12 22:45:42 -0300

    adição do botão ir para conteúdo principal

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -12,4 +12,15 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  focusOnMainContent(): void {
+    const mainTag = document.getElementsByTagName('main')[0];
+    
+    if(mainTag) {
+      mainTag.setAttribute('tabindex','0');
+      mainTag.focus();
+      setTimeout(() => {
+        mainTag.removeAttribute('tabindex');
+      }, 100);
+    }
+  }
 }
